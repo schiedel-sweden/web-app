6cfc1246bb2b70d480b34a8afee499bf2d87180c
Jonathan Partain <jonathan.partain@gmail.com>
2018-01-26 19:29:13 +0100

    Added functionality for the rebate buttons

--- src/views/PricePage/BoxRow.js
+++ src/views/PricePage/BoxRow.js
@@ -41,8 +41,10 @@ export default class BoxRow extends Component {
     async calcSum() {
         let number = this.state.antal;
         let price = this.state.pris;
+        // set rebate for all inidividual items
+        let rabatt = this.state.rabatt;
 
-        let totsum = number * price;
+        let totsum = number * price * ((100 - rabatt) / 100.0);
         await this.setState({
             sum: totsum,
         });
@@ -52,6 +54,13 @@ export default class BoxRow extends Component {
     }
 
 
+    rebateCallback = async (state) => {
+        await this.setState({
+            rabatt: state,
+        });
+        this.calcSum();
+    }
+
     callback = async (state) => {
         await this.setState({
             antal: state
@@ -59,7 +68,7 @@ export default class BoxRow extends Component {
     }
 
     callMe = () => {
-        this.props.parentCallback(this.state.sum, this.state.antal, this.state.number);
+        this.props.parentCallback(this.state.sum, this.state.antal, this.state.number, this.state.rabatt);
     }
 
 
@@ -99,7 +108,8 @@ export default class BoxRow extends Component {
                 {/* rabatt should only be able to be modified by one type of user I assume*/}
                 <div style={{flex: 0.13}}>
                     <GridBoxInc
-                        number={this.state.rabatt} />
+                        number={this.state.rabatt}
+                        parentCallback={this.rebateCallback} />
                 </div>
             </Row>
 
--- src/views/PricePage/ObjectSummarizer.js
+++ src/views/PricePage/ObjectSummarizer.js
@@ -39,12 +39,13 @@ export default class ObjectSummarizer extends Component {
     }
 
     // add callback functions
-    sendCallback = (sum, antal, number) => {
+    sendCallback = (sum, antal, number, rabatt) => {
         let stateMarker = this.state.rowItems;
         for(let i = 0; i < this.state.rowItems.length; i++) {
             if (number === this.state.rowItems[i].number) {
                 stateMarker[i].sum = sum;
                 stateMarker[i].antal = antal;
+                stateMarker[i].rabatt = rabatt;
                 this.setState({stateMarker});
             }
         }
@@ -55,7 +56,7 @@ export default class ObjectSummarizer extends Component {
         let netto = 0;
         let rowItems = this.state.rowItems;
         for(let i = 0; i < this.state.rowItems.length; i++) {
-            netto += rowItems[i].sum
+            netto += rowItems[i].sum;
         }
         let moms = netto / 4;
         // if moms is NaN, make it 0
@@ -95,7 +96,7 @@ export default class ObjectSummarizer extends Component {
                                         antal={item.antal}
                                         pris={item.pris}
                                         sum={item.sum}
-                                        rabatt={0}
+                                        rabatt={item.rabatt}
                                         parentCallback={this.sendCallback} />})}
                 </div>)
         }
